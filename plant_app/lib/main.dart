e9296af185574e80731c247bcf5fcfad7fa32073
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-22 14:58:25 +0200

    Some edited

--- plant_app/lib/main.dart
+++ plant_app/lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:plant_app/screen/home.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Home(),
     );
   }
