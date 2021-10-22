4deac5cd0c625e7be400fb73c8b1dc6dd2a3febe
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-22 21:01:09 +0200

    Added some features

--- plant_app/lib/screen/home.dart
+++ plant_app/lib/screen/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/componant/mydrawer.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,36 +13,64 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-        drawer: MyDrawer(),
+      //  drawer: const MyDrawer(),
         body: Stack(
-      children: [
-        Container(
-          height: double.infinity,
-          width: double.infinity,
-         color: const Color(0xff56b286),
-        ),
-        Positioned(
-          bottom: 0,
-          child: Container(
-            height: width,
-            width: width,
-            decoration: const BoxDecoration(color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30))),
-          ),
-        ),
-        Positioned(
-            top: width / 6,
-            right: - width / 1.3,
-            child: Container(
-
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("images/assets/ali.jpg"))),
-            ))
-      ],
-    ));
+          children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              color: const Color(0xff56b286),
+            ),
+            Column(mainAxisAlignment: MainAxisAlignment.start,
+             children: [
+              Padding(
+               padding: const EdgeInsets.symmetric(vertical:30),
+             child: Row(
+                children: [
+                  Row(
+                    children: [
+                      MyDrawer(),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                    Container(
+                      height: 60,
+                      width: 60,
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("images/assets/ali.jpg")),
+                          shape: BoxShape.circle),
+                    )
+                  ]),
+                ],
+              ),
+             )
+            ]),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                height: width / 1.2,
+                width: width,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30))),
+              ),
+            ),
+            Positioned(
+                top: 50,
+                right: -width / 1.3,
+                child: Container(
+                  height: 500,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("images/assets/plant.png"))),
+                ))
+          ],
+        ));
   }
 }
 /*
