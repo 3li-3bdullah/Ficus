83f7258fa2b7095041a5b37dc8a54e5a0cb1b512
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-22 21:00:40 +0200

    Some edited

--- plant_app/lib/componant/mydrawer.dart
+++ plant_app/lib/componant/mydrawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatefulWidget {
-  const MyDrawer({ Key? key }) : super(key: key);
+  // const MyDrawer({Key? key}) : super(key: key);
 
   @override
   _MyDrawerState createState() => _MyDrawerState();
@@ -12,59 +12,75 @@ class _MyDrawerState extends State<MyDrawer> {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
-        children: <Widget>[
+        children: [
           UserAccountsDrawerHeader(
-            accountName:const Text("Ali Abdullah"),
-            accountEmail: const Text("Alieko.A50@gmail.com"),
+            accountName: const Text("Ali Abdullah"),
+            accountEmail: const Text("Sudanese"),
             currentAccountPicture: Container(
               decoration: const BoxDecoration(
                   image: DecorationImage(
-                      image: AssetImage("example/images/slider/ali.jpg")),
+                      image: AssetImage("images/assets/ali.jpg")),
                   shape: BoxShape.circle),
             ),
             decoration: const BoxDecoration(
               color: Colors.white,
             ),
           ),
+         const Padding(
+            padding: EdgeInsets.symmetric(horizontal:20),
+            child: Divider(
+              color:Colors.grey
+            ),
+          ),
           drawerItems(
             "Profile",
             Icons.person_outline_outlined,
-            () {
-            },
+            () {},
           ),
           drawerItems(
             "Messages",
             Icons.message_outlined,
-            () {
-            },
+            () {},
           ),
           drawerItems(
             "Lists",
             Icons.list,
-            () {
-            },
+            () {},
           ),
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Divider(
               color: Colors.grey,
             ),
-          )
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children:[
+             drawerItems(
+            "Tell a Friend",
+            Icons.share,
+            () {},
+          ),
+          drawerItems(
+            "Help",
+            Icons.help_outline,
+            () {},
+          ), 
+            ]
+            )
         ],
       ),
     );
   }
 
-
-  
-
   InkWell drawerItems(String text, IconData icon, Function onTap) {
     return InkWell(
       child: ListTile(
-        title: Text(text, style: const TextStyle(color: Colors.black, fontSize: 18)),
+        title: Text(text,
+            style: const TextStyle(color: Colors.black, fontSize: 18)),
         leading: Icon(
-          Icons.person_outline_outlined,
-           color: Colors.grey, size: 25),
+          icon, 
+          color: Colors.grey, size: 25),
       ),
       onTap: () {
         onTap();
