f06cac037d18590246adcad7823cf6785cea4a33
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-22 21:56:12 +0200

    Removed the column

--- plant_app/lib/screen/home.dart
+++ plant_app/lib/screen/home.dart
@@ -13,64 +13,60 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      //  drawer: const MyDrawer(),
+        //  drawer: const MyDrawer(),
         body: Stack(
-          children: [
-            Container(
-              height: double.infinity,
-              width: double.infinity,
-              color: const Color(0xff56b286),
-            ),
-            Column(mainAxisAlignment: MainAxisAlignment.start,
-             children: [
-              Padding(
-               padding: const EdgeInsets.symmetric(vertical:30),
-             child: Row(
-                children: [
-                  Row(
-                    children: [
-                      MyDrawer(),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                    Container(
-                      height: 60,
-                      width: 60,
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("images/assets/ali.jpg")),
-                          shape: BoxShape.circle),
-                    )
-                  ]),
-                ],
-              ),
-             )
-            ]),
-            Positioned(
-              bottom: 0,
-              child: Container(
-                height: width / 1.2,
-                width: width,
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30))),
-              ),
-            ),
-            Positioned(
-                top: 50,
-                right: -width / 1.3,
-                child: Container(
-                  height: 500,
-                  decoration: const BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("images/assets/plant.png"))),
-                ))
-          ],
-        ));
+      children: [
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: const Color(0xff56b286),
+        ),
+        // Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        //   const SizedBox(height: 30),
+        //   Row(
+        //     children: [
+        //       Row(
+        //         children: [
+        //           MyDrawer(),
+        //         ],
+        //       ),
+        //       Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+        //         Container(
+        //           height: 60,
+        //           width: 60,
+        //           decoration: const BoxDecoration(
+        //               image: DecorationImage(
+        //                   image: AssetImage("images/assets/ali.jpg")),
+        //               shape: BoxShape.circle),
+        //         )
+        //       ]),
+        //     ],
+        //   )
+        // ]),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            height: width / 1.2,
+            width: width,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30))),
+          ),
+        ),
+        Positioned(
+            top: width * 1.2, // this will go down
+            right: width / 1.5,
+            child: Container(
+              height: width,
+              width: width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(width),
+                  color: Colors.indigo),
+            ))
+      ],
+    ));
   }
 }
 /*
@@ -113,4 +109,11 @@ Container(
           width: width,
           color: Colors.green,
         ),
+*/
+
+/*
+ height: 500,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("images/assets/plant.png"))), 
 */
