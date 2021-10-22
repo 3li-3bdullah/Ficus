ad1a11a2385ea45c7a194cd67509bc4cbedfcc6c
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-22 23:37:16 +0200

    Added more Edited

--- plant_app/lib/screen/home.dart
+++ plant_app/lib/screen/home.dart
@@ -12,8 +12,16 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-        //  drawer: const MyDrawer(),
+    return Scaffold(appBar: AppBar(
+      elevation: 0,
+      backgroundColor: const Color(0xff56b286),
+      actions: [Container(height: 60,width: 60,
+         decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage("images/assets/ali.jpg")),shape: BoxShape.circle,), 
+      )],
+    ),
+         drawer:  MyDrawer(),
         body: Stack(
       children: [
         Container(
@@ -21,6 +29,7 @@ class _HomeState extends State<Home> {
           width: double.infinity,
           color: const Color(0xff56b286),
         ),
+
         // Column(mainAxisAlignment: MainAxisAlignment.start, children: [
         //   const SizedBox(height: 30),
         //   Row(
@@ -56,14 +65,15 @@ class _HomeState extends State<Home> {
           ),
         ),
         Positioned(
-            top: width * 1.2, // this will go down
-            right: width / 1.5,
+            top: width / 2.2,
+            right: - width / 2,
             child: Container(
-              height: width,
-              width: width,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(width),
-                  color: Colors.indigo),
+              height: width / 1.5,
+              width: width / 2,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                          image: AssetImage("images/assets/plant.png"))
+                  ),
             ))
       ],
     ));
