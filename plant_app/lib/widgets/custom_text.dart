23775af029410b8ff81b86e7442b5d76d6c125e9
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-24 15:48:28 +0200

    Added color widget

--- plant_app/lib/widgets/custom_text.dart
+++ plant_app/lib/widgets/custom_text.dart
@@ -4,14 +4,15 @@ class CustomText extends StatelessWidget {
   const CustomText({
     required this.text,
     required this.fontsize,
-    required  this.fontweight,
+    required this.fontweight,required this.color,
   });
   final String text;
   final double fontsize;
   final FontWeight fontweight;
+  final Color color;
   @override
   Widget build(BuildContext context) {
     return Text(text,
-        style: TextStyle(fontSize: fontsize, fontWeight: fontweight));
+        style: TextStyle(fontSize: fontsize, fontWeight: fontweight,color:color));
   }
 }
