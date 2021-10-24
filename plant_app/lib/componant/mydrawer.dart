394e0ba175b5b601cdd0028e7a15fce35200a63d
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-24 13:58:01 +0200

    Declare instance variable

--- plant_app/lib/componant/mydrawer.dart
+++ plant_app/lib/componant/mydrawer.dart
@@ -10,71 +10,65 @@ class MyDrawer extends StatefulWidget {
 class _MyDrawerState extends State<MyDrawer> {
   @override
   Widget build(BuildContext context) {
+     bool _light = true;
     return Drawer(
-      child: ListView(
-        children: [
-          Container(
-              child: Padding(
-                  padding: const EdgeInsets.only(top: 30 , left:20),
-                  child: Column(              
-                    crossAxisAlignment: CrossAxisAlignment.start,    
-                    children: const [
-                        CircleAvatar(
-                          radius: 50,
-                          backgroundImage: AssetImage("images/assets/ali.jpg"),
-                        ),
-                        SizedBox(height: 20),
-                        Text("Ali Abdullah",
-                            style: TextStyle(
-                                fontSize: 22, fontWeight: FontWeight.w800)),
-                        SizedBox(height: 5),
-                        Text("Sudanese",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w400)),
-                      ]))),
-          const SizedBox(height: 18),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Divider(color: Colors.grey),
-          ),
-           drawerItems(
-            "Profile",
-            Icons.person_outline_outlined,
-            () {},
-          ),
-          drawerItems(
-            "Messages",
-            Icons.message_outlined,
-            () {},
-          ),
-          drawerItems(
-            "Lists",
-            Icons.list,
-            () {},
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Divider(
-              color: Colors.grey,
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children:[
-             drawerItems(
-            "Tell a Friend",
-            Icons.share,
-            () {},
-          ),
-          drawerItems(
-            "Help",
-            Icons.help_outline,
-            () {},
-          ),
-        ],
+        child: ListView(children: [
+      Padding(
+          padding: const EdgeInsets.only(top: 30, left: 20),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage("images/assets/ali.jpg"),
+                ),
+                SizedBox(height: 20),
+                Text("Ali Abdullah",
+                    style:
+                        TextStyle(fontSize: 22, fontWeight: FontWeight.w800)),
+                SizedBox(height: 5),
+                Text("Sudanese",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+              ])),
+      const SizedBox(height: 18),
+      const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Divider(color: Colors.grey),
+      ),
+      drawerItems(
+        "Profile",
+        Icons.person_outline_outlined,
+        () {},
+      ),
+      drawerItems(
+        "Messages",
+        Icons.message_outlined,
+        () {},
+      ),
+      drawerItems(
+        "Lists",
+        Icons.list,
+        () {},
+      ),
+      const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Divider(
+          color: Colors.grey,
+        ),
+      ),
+      
+      drawerItems(
+        "Tell a Friend",
+        Icons.share,
+        () {},
+      ),
+      drawerItems(
+        "Help",
+        Icons.help_outline,
+        () {},
       )
-          
-         ] ));
+    ]));
   }
 
   InkWell drawerItems(String text, IconData icon, Function onTap) {
